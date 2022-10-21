73a4b513497164c813f96336f88da79a3ada198e
Rajat <rajatraguvanshi3900@gmail.com>
2022-10-21 16:26:46 +0530

    Implementation of stack using STL

---
+++ STL-Stack.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    stack<int> st;
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    st.push(5);
+    st.push(6);
+    st.pop();
+    cout << "The top element is: " << st.top() << endl;
+    cout << "Is stack empty? " << st.empty() << endl;
+    cout << "The element present is stack are: " << endl;
+    while (!st.empty())
+    {
+        cout << st.top() << " ";
+        st.pop();
+    }
+    return 0;
+}
